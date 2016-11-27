781ddb79c9a54f25935f90b77694abd0f40d77a8
J Lee <jxl131130@utexas.edu>
2016-11-27 03:24:19 -0600

    Edit/More Code

--- ClientMain.java
+++ ClientMain.java
@@ -3,6 +3,7 @@ package assignment7;
 import java.io.*;
 import java.lang.reflect.Method;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 
@@ -14,8 +15,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.CustomMenuItem;
-import javafx.scene.control.Label; 
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.ScrollPane; 
 import javafx.scene.control.TextArea;
 
@@ -29,16 +32,20 @@ public class ClientMain extends Application{
 	private PrintWriter toServer;
 	private TextArea incoming;
 	private String serverName;
-	private String clientName; 
+	private String name; 
 	
 	private DataOutputStream dataToServer = null; 
 	private DataInputStream dataFromServer = null;
-
+	public MenuButton clientsOnline = new MenuButton();
+	public ArrayList<String> clientNameList = new ArrayList<String>();
+	public TextArea availableClientsText;
+	public Stage pS;
 
 	@Override // Override the start method in the Application class 
 	public void start(Stage primaryStage) { 
 		// Panel p to hold the label and text field 
 		
+		pS = primaryStage;
 		BorderPane paneForTextField = new BorderPane(); 
 		paneForTextField.setPadding(new Insets(5, 5, 5, 5)); 
 		paneForTextField.setStyle("-fx-border-color: green"); 
@@ -64,7 +71,27 @@ public class ClientMain extends Application{
             @Override
             public void handle(ActionEvent event) {
             	if(dataToServer != null && dataFromServer != null){
-            		toServer.println(outgoing.getText());
+            		int numSent = 0;
+            		String totalMessage = "";
+            		for(int i = 0; i < clientsOnline.getItems().size(); i++){
+            			CustomMenuItem m = (CustomMenuItem)clientsOnline.getItems().get(i);
+            			CheckBox c = (CheckBox)m.getContent();
+            			if(c.isSelected()){
+            				String clientName = clientNameList.get(i);
+            				totalMessage = totalMessage + clientName + ",";
+            				numSent++;
+            			}
+            		}
+            		
+            		String chatMessage = outgoing.getText();
+            		if(numSent == 0){
+            			totalMessage = "*sendToAll*" + chatMessage;
+            		}
+            		else{
+            			totalMessage = totalMessage + name + ","; // include the messenger as well
+            			totalMessage = totalMessage + "*privateMessages*" + chatMessage;
+            		}
+            		toServer.println(totalMessage);
             		toServer.flush();
             		outgoing.setText("");
             		outgoing.requestFocus();
@@ -82,6 +109,35 @@ public class ClientMain extends Application{
 		mainPane.setCenter(new ScrollPane(incoming)); 
 		mainPane.setTop(paneForTextField); 
 		mainPane.setBottom(chatTextField);
+		
+		availableClientsText = new TextArea();
+		availableClientsText.setPrefWidth(200);
+		availableClientsText.setPrefHeight(200);
+		availableClientsText.setEditable(false);
+		mainPane.setRight(new ScrollPane(availableClientsText));
+		
+		mainPane.setLeft(clientsOnline);
+		
+		/*
+		 * Create GROUP************
+		 */
+		Button groupButton = new Button("Create Group");
+		
+		groupButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	
+            }
+        });
+		
+		Button logOutButton = new Button("Log Out");
+		
+		logOutButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	
+            }
+        });
 		
 		// Create a scene and place it in the stage 
 		Scene scene = new Scene(mainPane, 500, 400); 
@@ -105,6 +161,7 @@ public class ClientMain extends Application{
 				dataFromServer = new DataInputStream(socket.getInputStream()); // create an input stream to receive data from server 
 				dataToServer = new DataOutputStream(socket.getOutputStream()); // create an output stream to send data to server
 
+				availableClientsText.appendText("Other People Online:\n");
 				//System.out.println("networking established");
 				Thread clientThread = new Thread(new IncomingReader(socket));
 				clientThread.start();
@@ -141,7 +198,33 @@ public class ClientMain extends Application{
 			String message;
 			try {
 				while ((message = fromServer.readLine()) != null) {
-					incoming.appendText(message + "\n");
+					if(message.startsWith("*peopleOnline*")){
+						//clientsOnline.getItems().clear();
+						//availableClientsText.setText("");
+						String[] peopleOnline = message.replace("*peopleOnline*", "").split(",");
+						for(int i = 0; i < peopleOnline.length; i++){
+							if(!clientNameList.contains(peopleOnline[i]) && !peopleOnline[i].equals(name)){
+								CheckBox checkBox = new CheckBox(peopleOnline[i]);
+								clientNameList.add(peopleOnline[i]);
+								CustomMenuItem newItem = new CustomMenuItem(checkBox);
+								newItem.setHideOnClick(false);
+								clientsOnline.getItems().add(newItem);
+								availableClientsText.appendText(peopleOnline[i] + "\n");
+							}
+						}
+					}
+					else if(message.startsWith("*setClientName*")){
+						String m = message.replace("*setClientName*", "");
+						name = m;
+						//pS.setTitle("Client: " + name);
+					}
+					else if(message.startsWith("*joinedGroup!*")){
+						String j = message.replace("*joinedGroup!*", "");
+						incoming.appendText(j + " has joined the chat!\n");
+					}
+					else{
+						incoming.appendText(message + "\n");
+					}
 				}
 			} catch (IOException ex) {
 				ex.printStackTrace();
--- ServerMain.java
+++ ServerMain.java
@@ -8,54 +8,119 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
-public class ServerMain implements Observer {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class ServerMain extends Application implements Observer {
+	private TextArea serverLog = new TextArea();
 	private ArrayList<PrintWriter> clientOutputStreams;
 	private ArrayList<Observable> clientList;
+	private ArrayList<String> clientNameList;
 	private int clientNum = 0;
 
 	public static void main(String[] args) {
-		try {
-			new ServerMain().setUpNetworking();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		launch(args);
 	}
 
-	private void setUpNetworking() throws Exception {
-		clientOutputStreams = new ArrayList<PrintWriter>();
-		clientList = new ArrayList<Observable>();
-		@SuppressWarnings("resource")
-		ServerSocket serverSock = new ServerSocket(4242);
-		while (true) {
-			Socket clientSocket = serverSock.accept();
-			
-			clientNum++;
-			
-			/*
-			// Display the client number 
-			System.out.println("Starting thread for client " + clientNum +
-						" at " + new Date() + '\n'); 
-			// Find the client's host name, and IP address 
-			InetAddress inetAddress = clientSocket.getInetAddress();
-			System.out.println("Client " + clientNum + "'s host name is "
-						+ inetAddress.getHostName() + "\n");
-			System.out.println("Client " + clientNum + "'s IP Address is " 
-						+ inetAddress.getHostAddress() + "\n");
-			*/
-			PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
-			clientOutputStreams.add(writer);
-			ClientHandler newHandler = new ClientHandler(clientSocket, clientNum);
-			Thread t = new Thread(newHandler);
-			clientList.add(newHandler);
-			
-			t.start();
-			System.out.println("got a connection");
-		}
-
+	@Override
+	public void start(Stage primaryStage){
+		serverLog.setEditable(false);
+		serverLog.setLayoutX(10);
+		serverLog.setLayoutY(10);
+		
+		Button buttonQuit = new Button("Quit Server");
+		buttonQuit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	System.exit(0);            
+            }
+        });
+		buttonQuit.setLayoutX(225);
+		buttonQuit.setLayoutY(215);
+		//ScrollPane scPane = new ScrollPane(serverLog);
+		Pane p = new Pane();
+		p.getChildren().add(serverLog);
+		p.getChildren().add(buttonQuit);
+		Scene scene = new Scene(p, 500, 260);
+		primaryStage.setTitle("Server");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		new Thread( () -> { 
+			try {  // Create a server socket 
+				clientOutputStreams = new ArrayList<PrintWriter>();
+				clientList = new ArrayList<Observable>();
+				clientNameList = new ArrayList<String>();
+				
+				@SuppressWarnings("resource")
+				ServerSocket serverSock = new ServerSocket(4242);
+				while (true) {
+					Socket clientSocket = serverSock.accept();
+					
+					clientNum++;
+					
+					/*
+					// Display the client number 
+					System.out.println("Starting thread for client " + clientNum +
+								" at " + new Date() + '\n'); 
+					// Find the client's host name, and IP address 
+					InetAddress inetAddress = clientSocket.getInetAddress();
+					System.out.println("Client " + clientNum + "'s host name is "
+								+ inetAddress.getHostName() + "\n");
+					System.out.println("Client " + clientNum + "'s IP Address is " 
+								+ inetAddress.getHostAddress() + "\n");
+					*/
+					PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+					clientOutputStreams.add(writer);
+					ClientHandler newHandler = new ClientHandler(clientSocket, clientNum);
+					Thread t = new Thread(newHandler);
+					clientList.add(newHandler);
+					
+					String newName = "" + clientNum;
+					clientNameList.add(newName);
+					
+					String outMessage = "*setClientName*" + newName;
+					writer.println(outMessage);
+					
+					String outwardMessage = "*peopleOnline*";
+					for(String s: clientNameList){
+						outwardMessage = outwardMessage + s + ",";
+					}
+					for (PrintWriter w : clientOutputStreams) {
+						w.println(outwardMessage);
+						w.flush();
+					}
+					
+					outwardMessage = "*joinedGroup!*" + newName;
+					for (PrintWriter wr : clientOutputStreams) {
+						wr.println(outwardMessage);
+						wr.flush();
+					}
+					
+					
+					
+					t.start();
+					serverLog.appendText("got a connection with " + newName + "\n");
+				}
+			} 
+			catch(IOException ex) { 
+				System.err.println(ex);
+			}
+		}).start();
+		
 	}
 
 	private void notifyClients(String message) {
@@ -68,10 +133,12 @@ public class ServerMain implements Observer {
 	class ClientHandler extends Observable implements Runnable {
 		private BufferedReader reader;
 		private int number;
+		private String name;
 
 		public ClientHandler(Socket clientSocket, int clientNum) throws IOException {
 			Socket sock = clientSocket;
 			this.number = clientNum;
+			this.name = "" + this.number;
 			reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 		}
 
@@ -79,8 +146,26 @@ public class ServerMain implements Observer {
 			String message;
 			try {
 				while ((message = reader.readLine()) != null) {
-					System.out.println("read " + message);
-					notifyClients("Client " + number + ": " + message);
+					if(message.startsWith("*sendToAll*")){
+						message = message.replace("*sendToAll*", "");
+						for(int i = 0; i < clientOutputStreams.size(); i++){
+							clientOutputStreams.get(i).println(this.name + ": " + message);
+							clientOutputStreams.get(i).flush();
+						}
+					}
+					else{
+						String[] messageParsed = message.split(",");
+						List<String> listParsed = Arrays.asList(messageParsed);
+						String actualMessage = messageParsed[messageParsed.length - 1].replace("*privateMessages*", "");
+						for(int i = 0; i < clientOutputStreams.size(); i++){
+							if(listParsed.contains(clientNameList.get(i))){
+								clientOutputStreams.get(i).println(this.name + ": " + actualMessage);
+								clientOutputStreams.get(i).flush();
+							}
+						}
+					}
+					serverLog.appendText("read " + message + "\n");
+					//notifyClients("Client " + number + ": " + message);
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -92,5 +177,4 @@ public class ServerMain implements Observer {
 	public void update(Observable o, Object arg) { // observer update
 		clientList.remove(o);
 	}
-
 }
